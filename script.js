19dfb9c5fd7162e4c12ec1ebba714f21ee76ce43
Skylar Dorn <skylardorn@Skylars-MacBook-Pro.local>
2022-06-28 19:33:39 -0700

    finished for loop and if statements.

--- script.js
+++ script.js
@@ -14,20 +14,45 @@ function writePassword() {
 }
 
 // create clickable button
-generateBtn.addEventListener("click", run)
+generateBtn.addEventListener("click", function run() {
 
-function run() {
-  length = parseInt("Choose the length of your password but, has to be within 8-128 characters.")
-    if (length > 8 && length <128) {
-      var parameters = {
+  var lengthQuestion = parseInt(prompt("Choose the length of your password but, has to be within 8-128 characters."));
+    if (lengthQuestion > 8 && lengthQuestion <128) {
+      var question = {
         specialCharacters: confirm ("**Special characters?**"),
-        uppercase: confirm ("**One uppercase letter?**"),
+        upperCase: confirm ("**One uppercase letter?**"),
         lowerCase: confirm ("**Lowercase?**"),
         numeric: confirm ("**Do you want to use numbers?**"),
 
+      };
+    };
+});
 
+var password ="";
 
-      }
-    }
+for (var i = 0; i length; i++) {
+  if (lengthQuestion.lowerCase) {
+    password += String.fromCharCode(Math.floor(Math.random() * 26) + 97 );
+    console.log(lengthQuestion);
+  }
 }
+
+  if (lengthQuestion.specialCharacters) {
+    password += String.fromCharCode(Math.floor(Math.random() * 26) + 97 );
+    console.log(lengthQuestion);
+  }
+
+
+  if (lengthQuestion.upperCase) {
+    password += String.fromCharCode(Math.floor(Math.random() * 26) + 65 );
+    console.log(lengthQuestion);
+  }
+
+
+  if (lengthQuestion.numeric) {
+    password += String.fromCharCode(Math.floor(Math.random() * 10) + 48 );
+    console.log(lengthQuestion);
+  }
+
+
 
